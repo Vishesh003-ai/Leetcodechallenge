3ffd40eb3e8ffb092d51d7d9a973a5772e23f0de
VISHESH CHOUDHARY <dimpykamboj272@gmail.com>
2026-08-02 11:29:26 +0530

    Time: 0 ms (100%), Space: 11.2 MB (56.23%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -15,7 +15,7 @@ public:
         if (pivotidx == -1) return true;
       if (count == 0) return true;
 
-        if (count > 1) return false;
-        return arr[n - 1] <= arr[0];
+        if (count <=1 && arr[n - 1] <= arr[0]) return true;
+        return false;
     }
 };
