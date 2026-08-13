db7c79ded6135ea6469f4775a9f6f967cc5b1ac1
VISHESH CHOUDHARY <dimpykamboj272@gmail.com>
2026-08-13 14:38:09 +0530

    Time: 0 ms (100%), Space: 10.5 MB (73.95%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,38 +1,40 @@
 class Solution {
 public:
+    bool possiblesoln(vector<int>&arr,int soln,int k){
+    int n=arr.size();
+    int sum=0;
+    int count=1;
+    for(int i=0;i<n;i++){
+     if(sum+arr[i]<=soln){
+        sum+=arr[i];
+     }
+     else{
+        sum=arr[i];
+        count++;
+     }
+
+    }
+    return count<=k;
+
+    }
 
 
-bool possiblesoln(vector<int>&arr,int soln,int k){
-      int n=arr.size();
-      int sum=0;
-      int count=1;
-      int adj=0;
-      if(n<k) return false;
-      for(int i=0;i<n;i++){
-          if (sum + arr[i] <= soln) 
-            sum += arr[i];
-         else {
-            count++;
-            sum = arr[i];
-        }
-        
-}
-return count <= k;
-  }
+
+
     int splitArray(vector<int>& arr, int k) {
-         int s=*max_element(arr.begin(),arr.end());
+        int s=*max_element(arr.begin(),arr.end());
         int e=accumulate(arr.begin(),arr.end(),0);
-        int ans=-1;
-        while(s<=e){
-            int mid=s+(e-s)/2;
-            if(possiblesoln(arr,mid,k)){
-                ans=mid;
-                e=mid-1;
-            }
-            else{
-                s=mid+1;
-            }
-        }
-        return ans;
+        int ans=0;
+     while(s<=e){
+      int m=s+(e-s)/2;
+      if(possiblesoln(arr,m,k)){
+        ans=m;
+        e=m-1;
+      }
+     else{
+        s=m+1;
+     }
+     }
+      return ans;
     }
 };
