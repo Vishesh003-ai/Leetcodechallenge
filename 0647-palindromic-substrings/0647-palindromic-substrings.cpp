85c59196a77c3b27db082d4569f2d5d43cadd1d8
VISHESH CHOUDHARY <dimpykamboj272@gmail.com>
2026-08-23 18:25:39 +0530

    Time: 3 ms (85.63%), Space: 8.6 MB (78.06%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+   int palindrome(string &s,int i,int j){
+    int count=0;
+     while(i>=0&&j<s.length()&& s[i]==s[j]){
+        count++;
+        i--;
+        j++;
+     }
+     return count;
+   }
+    int countSubstrings(string s) {
+        int count=0;
+        for(int center=0;center<s.length();center++){
+         //odd
+         int i=center;
+         int j=center;
+         int oddlen=palindrome(s,i,j);
+         //even
+          i=center;
+         j=center+1;
+         int evenlen=palindrome(s,i,j);
+         count+=oddlen+evenlen;
+        }
+         
+        return count;
+    }
+};
