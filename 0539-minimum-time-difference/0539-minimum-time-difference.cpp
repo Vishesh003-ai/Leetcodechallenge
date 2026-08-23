e822d9072a45403bc19eb1cacaf76798b6a039de
VISHESH CHOUDHARY <dimpykamboj272@gmail.com>
2026-08-23 14:47:54 +0530

    Time: 0 ms (100%), Space: 17.7 MB (98.78%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.cpp
+++ 0539-minimum-time-difference/0539-minimum-time-difference.cpp
@@ -1,23 +1,44 @@
 class Solution {
 public:
-   int converttomin(string &time){
-    int hr=stoi(time.substr(0,2));
-    int min=stoi(time.substr(3,2));
-    return 60*hr+min;
+    int convertToMin(string &time) {
+        int hr = (time[0] - '0') * 10 + (time[1] - '0');
+        int mn = (time[3] - '0') * 10 + (time[4] - '0');
+
+        return hr * 60 + mn;
     }
+
     int findMinDifference(vector<string>& timePoints) {
-        vector<int>mins;
-        for(auto time:timePoints){
-         mins.push_back(converttomin(time));}
-         sort(mins.begin(),mins.end());
-         int ans=INT_MAX;
-         for(int i=0;i<mins.size()-1;i++){
-            ans=min(ans,mins[i+1]-mins[i]);
-         }
-         int lastdiff=mins[0]+1440-mins[mins.size()-1];
-         ans=min(ans,lastdiff);
-         return ans;
+        vector<bool> seen(1440, false);
+
+        for (auto &time : timePoints) {
+            int t = convertToMin(time);
+
+            // Duplicate time
+            if (seen[t])
+                return 0;
+
+            seen[t] = true;
         }
-        
-    
+
+        int first = -1;
+        int prev = -1;
+        int ans = INT_MAX;
+
+        for (int i = 0; i < 1440; i++) {
+            if (seen[i]) {
+                if (first == -1)
+                    first = i;
+
+                if (prev != -1)
+                    ans = min(ans, i - prev);
+
+                prev = i;
+            }
+        }
+
+        // Circular difference: last -> first next day
+        ans = min(ans, first + 1440 - prev);
+
+        return ans;
+    }
 };
