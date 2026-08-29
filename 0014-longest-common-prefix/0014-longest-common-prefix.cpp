04537e7efd1fa91d975544c08b1175191d1064a5
VISHESH CHOUDHARY <dimpykamboj272@gmail.com>
2026-08-29 18:21:09 +0530

    Time: 0 ms (100%), Space: 11.9 MB (58.34%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-        string ans = "";
-        int n = strs.size();
-    for(int i=0;i<strs[0].size();i++){
-        char ch = strs[0][i];
-        bool match = true;
-        for(int j=1;j<n;j++){
-            if(strs[j].size() < i || ch != strs[j][i]){
-                match = false;
-                break;
+    string longestCommonPrefix(const vector<string>& strs) {
+        if (strs.empty()) {
+            return "";
+        }
+        for (int i = 0; i < strs[0].size(); i++) {
+            char ch = strs[0][i];
+            for (int j = 1; j < strs.size(); j++) {
+                if (i >= strs[j].size() || strs[j][i] != ch) {
+                    return strs[0].substr(0, i);
+                }
             }
         }
-        if(match == false){
-            break;
-        }else{
-            ans.push_back(ch);
-        }
-    }
-    return ans;
+
+        return strs[0];
     }
 };
