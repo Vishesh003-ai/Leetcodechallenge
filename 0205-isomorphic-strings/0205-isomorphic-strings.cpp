a4eccfde38def00bcd0fe6957f141951f1e29837
VISHESH CHOUDHARY <dimpykamboj272@gmail.com>
2026-08-30 09:17:24 +0530

    Time: 0 ms (100%), Space: 9 MB (98.83%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-   string normalise(unordered_map<char,int>&mapping,string &st){
-char start='a';
-for(int i=0;i<st.length();i++){
-  char ch=st[i];
-  if(mapping.find(ch)==mapping.end()){
-    mapping[ch]=start;
-    //ch=start;
-    start++;
-  }
-}
-for (int i = 0; i < st.length(); i++) {
-            st[i] = mapping[st[i]];
-        }
-  return st;
-   }
-    bool isIsomorphic( string& s,  string& t) {
-        // Implement logic to check if `s` and `t` are isomorphic
-       unordered_map<char,int>mapping1;
-       unordered_map<char,int>mapping2;
-       string s1=normalise(mapping1,s);
-        string s2=normalise(mapping2,t);
+    void normalise(string &st) {
+        char start = 'a';
+        char mapping[256] = {0}; // Fast array instead of unordered_map
 
-        return s1==s2;
-        
+        for (int i = 0; i < st.length(); i++) {
+            char ch = st[i];
+            if (mapping[ch] == 0) {
+                mapping[ch] = start;
+                start++;
+            }
+            st[i] = mapping[ch]; // Normalize in a single loop
+        }
+    }
+
+    bool isIsomorphic(string s, string t) {
+        normalise(s);
+        normalise(t);
+        return s == t;
     }
 };
