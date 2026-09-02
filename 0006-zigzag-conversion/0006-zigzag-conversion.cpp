8271d9663eb3142d681f9ba917f5e01665c08f22
VISHESH CHOUDHARY <dimpykamboj272@gmail.com>
2026-09-02 16:54:43 +0530

    Time: 5 ms (51.1%), Space: 14 MB (57.25%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if(numRows==1) return s;
+        vector<string>zigzag(numRows);
+        bool direction=1;
+        int i=0,row=0;
+        while(true){
+        if(direction){
+            while(row<numRows&& i<s.size()){
+                zigzag[row++].push_back(s[i++]);
+            }
+            row=numRows-2;
+        }
+  else{
+    while(row>=0&&i<s.size()){
+        zigzag[row--].push_back(s[i++]);
+    }
+row=1;
+  }
+   direction=!direction;
+   if(i>=s.length()) break;
+        }
+        string ans="";
+   for(int i=0;i<zigzag.size();i++){
+    ans+=zigzag[i];
+   }
+return ans;
+    }
+};
