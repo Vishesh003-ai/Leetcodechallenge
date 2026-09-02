e863fafddeb751c8ed20d4f1d53378d49723468f
VISHESH CHOUDHARY <dimpykamboj272@gmail.com>
2026-09-02 21:36:12 +0530

    Time: 1466 ms (5.42%), Space: 451.1 MB (15.31%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -1,47 +1,16 @@
 class Solution {
 public:
-
-    int val(char c) {
-        if (c == 'A') return 0;
-        if (c == 'C') return 1;
-        if (c == 'G') return 2;
-        return 3; // T
-    }
-
     vector<string> findRepeatedDnaSequences(string s) {
-
-        vector<string> ans;
-
-        if (s.length() < 10)
-            return ans;
-
-        unordered_map<int, int> mp;
-
-        int code = 0;
-
-        // First 10 characters
-        for (int i = 0; i < 10; i++) {
-            code = (code << 2) | val(s[i]);
-        }
-
-        mp[code]++;
-
-        // Sliding window
-        for (int i = 10; i < s.length(); i++) {
-
-            // Remove the oldest 2 bits
-            code &= (1 << 18) - 1;
-
-            // Add new character
-            code = (code << 2) | val(s[i]);
-
-            mp[code]++;
-
-            if (mp[code] == 2) {
-                ans.push_back(s.substr(i - 9, 10));
+        unordered_map<string,int>mp;
+        vector<string>ans;
+        for(int i=0;i+10<=s.length();i++){
+  string temp=s.substr(i,10);//takes 10 char including i
+  mp[temp]++;
+  if (mp[temp] == 2) {
+                ans.push_back(temp);
             }
-        }
 
+        }
         return ans;
     }
 };
