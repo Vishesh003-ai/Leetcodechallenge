589764c8868811497dd7cc14a45ebbe63a71a5fb
VISHESH CHOUDHARY <dimpykamboj272@gmail.com>
2026-09-04 20:28:29 +0530

    Time: 71 ms (85.43%), Space: 8.9 MB (90.75%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -2,26 +2,25 @@ class Solution {
 public:
     int beautySum(string s) {
         int sum = 0;
-
         for (int i = 0; i < s.length(); i++) {
-            int freq[26] = {0};//Reset happens here for new substring 
-
+            int freq[26] = {0};
+            int maxFreq = 0;
             for (int j = i; j < s.length(); j++) {
+                // Add current character
                 freq[s[j] - 'a']++;
+                // Update maximum frequency
+                maxFreq = max(maxFreq, freq[s[j] - 'a']);
 
-                int maxFreq = 0;
+                // Find minimum non-zero frequency
                 int minFreq = INT_MAX;
-
                 for (int k = 0; k < 26; k++) {
                     if (freq[k] > 0) {
-                        maxFreq = max(maxFreq, freq[k]);
                         minFreq = min(minFreq, freq[k]);
                     }
                 }
                 sum += maxFreq - minFreq;
             }
         }
-
         return sum;
     }
 };
