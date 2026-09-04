7d21441540d964a5665b601bbec9d5cc522d80e7
VISHESH CHOUDHARY <dimpykamboj272@gmail.com>
2026-09-04 20:23:00 +0530

    Time: 1 ms (85.44%), Space: 14.6 MB (72.26%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -3,12 +3,12 @@ public:
     int firstUniqChar(string s) {
         int freq[26] = {0};
 
-        // Count frequency
+   
         for (char ch : s) {
             freq[ch - 'a']++;
         }
 
-        // Find first unique character
+        
         for (int i = 0; i < s.length(); i++) {
             if (freq[s[i] - 'a'] == 1) {
                 return i;
