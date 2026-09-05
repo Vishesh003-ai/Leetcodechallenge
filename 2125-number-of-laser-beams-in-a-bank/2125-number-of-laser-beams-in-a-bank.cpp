6b8c47bd5dcc9982743cb015ab7d2ea3e4ef01c3
VISHESH CHOUDHARY <dimpykamboj272@gmail.com>
2026-09-05 10:22:18 +0530

    Time: 0 ms (100%), Space: 27.3 MB (83.14%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
-        int laser=0;
-        int prev=0;
-        for(string &row:bank){
-          int curr=0;
-          for(char ch:row){
-            if(ch=='1'){
-                curr++;
-            }
-          }
-if(curr>0){
-            laser+=curr*prev;
-            prev=curr;
+        int laser = 0;
+        int prev = 0;
+        // Pass by const reference to prevent copying strings
+        for (const string& row : bank) {
+            // std::count is heavily optimized and replaces your inner loop
+            int curr = count(row.begin(), row.end(), '1');
+            if (curr > 0) {
+                laser += curr * prev;
+                prev = curr;
             }
         }
         return laser;
