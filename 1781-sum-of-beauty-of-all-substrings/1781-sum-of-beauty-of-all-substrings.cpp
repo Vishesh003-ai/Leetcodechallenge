f25608c60c57163459612d8944ff544a4bed6311
VISHESH CHOUDHARY <dimpykamboj272@gmail.com>
2026-09-05 08:32:44 +0530

    Time: 75 ms (84.71%), Space: 8.9 MB (90.65%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     int beautySum(string s) {
-        int sum = 0;
-        for (int i = 0; i < s.length(); i++) {
-            int freq[26] = {0};
-            int maxFreq = 0;
-            for (int j = i; j < s.length(); j++) {
-                // Add current character
-                freq[s[j] - 'a']++;
-                // Update maximum frequency
-                maxFreq = max(maxFreq, freq[s[j] - 'a']);
+        int sum=0;
+        for(int i=0;i<s.length();i++){
+            int freq[26]={0};
+           int maxf=INT_MIN;
+          for(int j=i;j<s.length();j++){
+             freq[s[j]-'a']++;
 
-                // Find minimum non-zero frequency
-                int minFreq = INT_MAX;
-                for (int k = 0; k < 26; k++) {
-                    if (freq[k] > 0) {
-                        minFreq = min(minFreq, freq[k]);
-                    }
+             maxf=max(maxf,freq[s[j]-'a']);
+             int minf=INT_MAX;
+             for(int k=0;k<26;k++){
+                if(freq[k]>0){
+                    minf=min(minf,freq[k]);
                 }
-                sum += maxFreq - minFreq;
-            }
+             }
+               sum+=(maxf-minf);
+          }
+       
         }
         return sum;
     }
