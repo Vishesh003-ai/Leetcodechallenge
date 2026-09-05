f7c97af493bc7db5640928f11f67257840c8a280
VISHESH CHOUDHARY <dimpykamboj272@gmail.com>
2026-09-05 10:19:13 +0530

    Time: 14 ms (16.03%), Space: 33.1 MB (18.7%) - LeetHub

---
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numberOfBeams(vector<string>& bank) {
+        int laser=0;
+        int prev=0;
+        for(string row:bank){
+          int curr=0;
+          for(char ch:row){
+            if(ch=='1'){
+                curr++;
+            }
+          }
+if(curr>0){
+            laser+=curr*prev;
+            prev=curr;
+            }
+        }
+        return laser;
+    }
+};
