68dfb2457e251a2164cdbfe57970ad4960d61ddf
VISHESH CHOUDHARY <dimpykamboj272@gmail.com>
2026-09-11 10:11:01 +0530

    Time: 3 ms (12.93%), Space: 13.6 MB (91.85%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        int n = chars.size();
-        int index = 0;
-        for(int i = 0; i < n; ) {
-            char ch = chars[i];
-            int count = 0;
-            // Count consecutive same characters
-            while(i < n && chars[i] == ch) {
-                count++;
-                i++;
-            }
-            // Put character
-            chars[index++] = ch;
-            // Put frequency if > 1
-            if(count > 1) {
-                string s = to_string(count);
-                for(char c : s) {
-                    chars[index++] = c;
-                }
-            }
+        int n=chars.size();
+        int index=0;
+        for(int i=0;i<n;){
+          int count=0;
+          char ch=chars[i];
+          while(i<n&&chars[i]==ch){
+            i++;
+            count++;
+          }
+        chars[index++]=ch;
+        if(count>1){
+     string s=to_string(count);
+     for(char c:s){
+        chars[index++]=c;
+     }
+        }
+
+
+
         }
         return index;
     }
