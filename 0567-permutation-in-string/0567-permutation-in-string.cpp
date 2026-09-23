ab22f5a5669834b5020ccfeec0832667eaa03e3b
VISHESH CHOUDHARY <dimpykamboj272@gmail.com>
2026-09-23 19:26:44 +0530

    Time: 0 ms (100%), Space: 9.8 MB (89.38%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int n1 = s1.length();
-        int n2 = s2.length();
-        if (n1 > n2) return false;
-        int mp[256] = {0};
-        for (char c : s1) {
-            mp[c]++;
-        }
+        if (s1.size() > s2.size()) return false;
+        int mp[26] = {0};
+        for (char c : s1)
+            mp[c - 'a']++;
         int i = 0;
-        int j = n1 - 1;
-        while (j < n2) {
-            int temp[256] = {0};
-            for (int k = i; k <= j; k++) {
-                temp[s2[k]]++;
+        for (int j = 0; j < s2.size(); j++) {
+            mp[s2[j] - 'a']--;
+            if (j - i + 1 > s1.size()) {
+                mp[s2[i] - 'a']++;
+                i++;
             }
-            bool found = true;
-            for (int k = 0; k < 256; k++) {
-                if (mp[k] != temp[k]) {
-                    found = false;
-                    break;
+            if (j - i + 1 == s1.size()) {
+                bool ok = true;
+                for (int k = 0; k < 26; k++) {
+                    if (mp[k] != 0) {
+                        ok = false;
+                        break;
+                    }
                 }
+                if (ok) return true;
             }
-            if (found) return true;
-            i++;
-            j++;
         }
         return false;
     }
